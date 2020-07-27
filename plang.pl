6377f7c53da99b67c3b10f02a6eb0cb6c641cd63
Kai Carver <kaicarver@gmail.com>
2020-07-28 00:24:22 +0200

    get term; use named captures

--- plang.pl
+++ plang.pl
@@ -4,15 +4,14 @@
 # usage:
 #   perl plang.pl Rye.wikipedia.html
 use 5.010;
+use List::Util qw(any);
 
 $/ = undef;
 while (<>) {
   if (/<nav id="p\-lang.*?<ul class="vector-menu-content-list">(.*?)<\/ul>/s) {
     $list = $1;
-    while ($list =~ /(<a href="https:\/\/(.*?)\..*?>(.*?)<\/a>)/g) {
-      $lang = $2;
-      $term = $3;
-      say "$lang: $term" if $lang =~ /^(en|de|fr|es|zh)$/;
+    while ($list =~ /(<a href="https:\/\/(?<lang>.*?)\..*?" title="(?<term>.*?) – .*?>(?<language>.*?)<\/a>)/g) {
+      say "$+{lang}: $+{term}" if any {$_ eq $+{lang}} qw/en fr de es zh/;
     }
   }
 }
